a0258f013832f7bd3848f49212a2f11bf71ab151
vinicius martin <viniciusgulartemartin@gmail.com>
2019-01-31 12:52:51 -0200

    add configs

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import App from './App';
+import App from './components/app';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "ENTER YOURS HERE",
-  authDomain: "ENTER YOURS HERE",
-  databaseURL: "ENTER YOURS HERE",
-  projectId: "ENTER YOURS HERE",
-  storageBucket: "ENTER YOURS HERE",
-  messagingSenderId: "ENTER YOURS HERE"
+  apiKey: "AIzaSyCpyFnDOeTi8R0aOqythf20SdsygsMf65A",
+  authDomain: "todoapp-d740f.firebaseapp.com",
+  databaseURL: "https://todoapp-d740f.firebaseio.com",
+  projectId: "todoapp-d740f",
+  storageBucket: "todoapp-d740f.appspot.com",
+  messagingSenderId: "413317750782"
+
 }
 firebase.initializeApp(config);
 const databaseRef = firebase.database().ref();
