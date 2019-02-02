e14449bfe87dff4691a5026b3dcec1a86fa151dd
Vinicius Martin <viniciusgulartemartin@gmail.com>
2019-02-02 19:09:00 -0200

    fix path file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
-import App from './components/app';
+import App from './components/app/app';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
