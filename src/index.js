a6f63bceeb0ca6fedf8f288107eeb383fca71d41
Vinicius Martin <viniciusgulartemartin@gmail.com>
2019-02-04 08:24:00 -0200

    add sass mixins

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './components/app/app';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
